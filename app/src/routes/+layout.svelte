132cfaad306f83fcef87124c4cf424a5fc393d2b
Adam Jace <aajaces@gmail.com>
2025-11-14 18:42:20 -0600

    Update +layout.svelte

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
+	import { dev } from "$app/environment";
 	import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+	injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
